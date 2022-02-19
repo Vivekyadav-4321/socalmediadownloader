38abe7dddae2870df28620026a4ee54c965a2197
vivek <smartvivekyadav9@gmail.com>
2022-02-19 17:21:10 +0530

    this is app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const PORT = 5000 || process.env.PORT
+
+app.get("/",(req, res)=>{
+
+res.send("hi this is working fine")
+
+
+})
+
+app.listen(PORT,()=>{console.log("sever is live");})
