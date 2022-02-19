6a27174945a4787d00cba6ee79a165e262c42e68
vivek <smartvivekyadav9@gmail.com>
2022-02-19 20:19:21 +0530

    sdf

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const portnumber = process.env.PORT || 5000
 
 app.get("/home",(req, res)=>{
+    res.end("hi");
+    })
+    
+app.get("/home",(req, res)=>{
 res.end("hi");
 })
 
